8356d6aa2c89a7ef1e5466e3e7a611bc61be241f
princecodes247 <princecodes247@gmail.com>
2021-07-15 09:29:06 -0400

    feat: add more methods

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ function Queue(concurrentCount = 1) {
   this.threads = 1;
   this.todo = [];
   this.currentTask = {};
+  this.stop = false;
   this.add = (func, args) => {
     _this.todo.push({ func, args });
+    _this.start();
+  };
+  this.rerun = () => {
+    _this.stop = true;
+    _this.todo = [{func: CipherType.prototype._clear, args: ""}, ..._this.complete, ..._this.todo];
     _this.start();
   };
   this.execute = async () => {
@@ -19,38 +25,43 @@ function Queue(concurrentCount = 1) {
   this.runNext = function () {
     return _this.running.length < _this.threads && _this.todo.length > 0;
   };
-  this.run = async function () {
+  this.run = function () {
     _this.idle = false;
-    while (_this.runNext()) {
-      _this.currentTask = _this.todo.shift();
-      _this.currentTask.func(..._this.currentTask.args).then(() => {
-        _this.complete.push(_this.running.shift());
-        _this.run();
-      });
-      _this.running.push(_this.currentTask);
-    }
-    _this.idle = true;
+      while (_this.runNext()) {
+        _this.currentTask = _this.todo.shift();
+        _this.currentTask.func(..._this.currentTask.args).then(() => {
+          _this.complete.push(_this.running.shift());
+          _this.run();
+        });
+        _this.running.push(_this.currentTask);
+      }
+
     // }
   };
   this.start = () => {
     if (_this.idle) {
+      _this.stop = false;
       _this.run();
     }
   };
 }
 
-function CipherType(target, options = { startSpeed: 200 }) {
-  var _this = this;
+function CipherType(target, options = { startSpeed: 200, breakLine: true }) {
+  const _this = this;
   this.letters = "∀∃ƂOo⅂AɌFWDU∋IßP⅁XꝚ";
   this.symbols = "Ɣ%A$D#Fɻ∀∃∂⅄@W⅂&ꝚǶs∇!U∋Iß℘P⅁X∆";
   this.result = "";
   this.startSpeed = options.startSpeed;
+  this.breakLine = true;
   this.speed = 100;
+  this.nextStringDelay = 100;
   this.target = () => document.querySelector(target);
   const cipherQueue = new Queue();
   // let todo = [];
   const actions = {};
   actions.errors = [];
+
+  //WRITE CODE FOR TYPING ELEMENT HARD CODED TEXT
 
   this.sleep = async (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -64,19 +75,29 @@ function CipherType(target, options = { startSpeed: 200 }) {
     return str.slice(0, -1);
   };
 
-  this.oneByOne = async (text, count, speed, useSymbols = false) => {
+  this.oneByOne = async (
+    texts = ["cipherType"],
+    count,
+    speed,
+    useSymbols = false
+  ) => {
     let resc = useSymbols ? this.symbols : this.letters;
 
-    for (let a = 0; a < text.length; a++) {
-      let final = text[a];
-      let past = _this.target().innerHTML;
-      for (let i = 0; i < count; i++) {
-        await this.sleep(speed);
-        _this.target().innerHTML = past + this.symbolGenerator(resc);
+    for (let j = 0; j < texts.length; j++) {
+      for (let a = 0; a < texts[j].length; a++) {
+        let final = texts[j][a];
+        let past = _this.target().innerHTML;
+        for (let i = 0; i < count; i++) {
+          if (texts[j][a] === " ") {
+            continue;
+          }
+          await this.sleep(speed);
+          _this.target().innerHTML = past + this.symbolGenerator(resc);
+        }
+        _this.target().innerHTML = past + final;
       }
-      _this.target().innerHTML = past + final;
+      _this.target().innerHTML += _this.breakLine ? "<br/>" : " ";
     }
-
     await this.sleep(speed * 5);
   };
 
@@ -85,29 +106,35 @@ function CipherType(target, options = { startSpeed: 200 }) {
 
     let past = _this.target().innerHTML;
 
-      for (let i = 0; i < 20; i++) {
-        let result = "";
-        for (let a = 0; a < text.length; a++) {
-          result += text[a] === " " ? " " : _this.symbolGenerator(resc);
-        }
-        _this.target().innerHTML = past + result;
-        await this.sleep(speed);
+    for (let i = 0; i < 20; i++) {
+      let result = "";
+      for (let a = 0; a < text.length; a++) {
+        result += text[a] === " " ? " " : _this.symbolGenerator(resc);
       }
-      _this.target().innerHTML = past + text;
-      await this.sleep(speed * 3);
-    
+      _this.target().innerHTML = past + result;
+      await this.sleep(speed);
+    }
+    _this.target().innerHTML = past + text;
+    await this.sleep(speed * 3);
   };
 
-  CipherType.prototype._write = async (text) => {
+  CipherType.prototype._write = async (texts, count, speed, useSymbols) => {
+    if (typeof texts === "string") {
+      texts = [texts];
+    }
     await _this.sleep(_this.startSpeed);
-    await _this.allAtOnce(text, 4, 200, true);
+    await _this.oneByOne(texts, count, speed, useSymbols);
     return this;
   };
 
   CipherType.prototype._delete = async (len, speed = _this.speed) => {
     await _this.sleep(_this.startSpeed);
-    if (typeof(len) === "string") {
+    console.log(typeof len);
+    if (typeof len === "string") {
       len = len === "CLEAR" ? _this.target().innerHTML.length : 0;
+    }
+    if (typeof len === "undefined") {
+      len = _this.target().innerHTML.length;
     }
     //let len = _this.target().innerHTML.length;
     for (let a = 0; a < len; a++) {
@@ -119,34 +146,69 @@ function CipherType(target, options = { startSpeed: 200 }) {
     return this;
   };
 
-  CipherType.prototype.type = (text) => {
-    cipherQueue.add(CipherType.prototype._write, [text]);
+  CipherType.prototype.type = (
+    text,
+    count = 4,
+    speed = 80,
+    useSymbols = false
+  ) => {
+    cipherQueue.add(CipherType.prototype._write, [
+      text,
+      count,
+      speed,
+      useSymbols,
+    ]);
     return this;
   };
   CipherType.prototype.backspace = (len, speed = _this.speed) => {
     cipherQueue.add(CipherType.prototype._delete, [len, speed]);
     return this;
   };
-  CipherType.prototype.clear = (speed = _this.speed) => {
-
-    cipherQueue.add(CipherType.prototype._delete, [len, speed]);
+  CipherType.prototype._clear = async (speed = _this.speed) => {
+      _this.target().innerHTML = "";
     return this;
   };
+  CipherType.prototype.pause = (ms) => {
+    async function _(delay) {
+      await this.sleep(delay);
+    }
+    cipherQueue.add(_, ms);
+    return this;
+  };
+  CipherType.prototype.break = () => {
+    async function _() {
+      _this.target().innerHTML = _this.target().innerHTML + "<br />";
+    }
+    cipherQueue.add(_, "");
+    return this;
+  };
+  CipherType.prototype.empty = () => {
+
+    cipherQueue.add(CipherType.prototype._clear, "");
+    return this;
+  };
+  CipherType.prototype.loop = () => {
+    cipherQueue.add(CipherType.prototype._delete, []);
+    cipherQueue.rerun();
+    return this;
+  };
+  CipherType.prototype.reset = () => {
+    cipherQueue.rerun();
+    return this;
+  };
+  //ADD OPTIONS METHOD
   return this;
 }
 
 let type = new CipherType("h1");
 
 type
-  .type("Welcome to the future")
-  .backspace(10, 150)
-  .type("Crypto")
-  .backspace("CLEAR", 50)
-  .type("Ways to go")
-  .backspace("CLEAR")
-  .type("One step at a time")
-//.then(res => res.delete(10, 50).then(res => res.type("CRYPTo")))
-//CryptoType.oneByOne(text, target, 4, 80, true);
+  .type(["HI Ebi", "Yo!!!"], 3, 40)
+  .backspace()
+  .type("What's up?")
+  .type("What's up?");
 
-//oneByOne(text, target, count, speed, useSymbols);
-//allAtOnce(text, target, 10, 600, symbols);
+let btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  type.reset();
+});
